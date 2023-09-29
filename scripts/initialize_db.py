cbb689d6b9ce50f5017787993ce7f68e325bf4e5
peterberekvolgyi <berekvolgyipeter@gmail.com>
2023-09-29 15:49:50 +0200

    update scripts

---
+++ scripts/initialize_db.py
@@ -0,0 +1,12 @@
+from app.database import utils as db_utils
+from app.schemas.drivers import DriversIn
+from app.schemas.results import ResultsIn
+from scripts.initial_data import DRIVERS, RESULTS
+
+
+if __name__ == "__main__":
+    for driver in DRIVERS:
+        db_utils.add_driver(DriversIn(**driver))
+
+    for result in RESULTS:
+        db_utils.add_result(ResultsIn(**result))
