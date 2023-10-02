03e819f228bbd4982b5373a762c7c6120758afb0
peterberekvolgyi <berekvolgyipeter@gmail.com>
2023-10-02 17:10:50 +0200

    implementing tests

--- tests/unit/routes/test_drivers.py
+++ tests/unit/routes/test_drivers.py
@@ -2,14 +2,16 @@ from unittest.mock import patch
 
 
 @patch("app.routes.drivers.interactor.list_drivers", return_value={"status": "OK"})
-def test_list_drivers(mock_list_drivers, mock_client):
+def test_list_drivers(mock_interactor, mock_client):
     response = mock_client.get("/drivers?name=Mika%20Hakkinen")
+    assert mock_interactor.call_count == 1
     assert response.json == {"status": "OK"}
     assert response.status_code == 200
 
 
 @patch("app.routes.drivers.interactor.add_driver", return_value={"status": "OK"})
-def test_add_driver(mock_list_drivers, mock_client):
+def test_add_driver(mock_interactor, mock_client):
     response = mock_client.post("/drivers", json={"name": "Mika Hakkinen", "team": "McLaren Mercedes"})
+    assert mock_interactor.call_count == 1
     assert response.json == {"status": "OK"}
     assert response.status_code == 200
--- tests/unit/routes/test_results.py
+++ tests/unit/routes/test_results.py
@@ -2,15 +2,17 @@ from unittest.mock import patch
 
 
 @patch("app.routes.results.interactor.list_results", return_value={"status": "OK"})
-def test_list_results(mock_list_results, mock_client):
+def test_list_results(mock_interactor, mock_client):
     response = mock_client.get("/results?driver_name=Mika%20Hakkinen")
+    assert mock_interactor.call_count == 1
     assert response.json == {"status": "OK"}
     assert response.status_code == 200
 
 
 @patch("app.routes.results.interactor.add_result", return_value={"status": "OK"})
-def test_add_result(mock_list_results, mock_client):
+def test_add_result(mock_interactor, mock_client):
     response = mock_client.post("/results", json={
         "driver_name": "Mika Hakkinen", "grand_prix": "1998 Australia", "position": 1})
+    assert mock_interactor.call_count == 1
     assert response.json == {"status": "OK"}
     assert response.status_code == 200
